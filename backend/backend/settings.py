7b823144146f6d45190ee09a4cd075ad1eac525c
Amulya Jain <jainamulyawin@gmail.com>
2024-09-29 23:33:14 +0530

    Update complete codes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgressql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PWD"),
-        'HOST': os.getenv("HOST"),
+        'HOST': os.getenv("DB_HOST"),
         'PORT': os.getenv("DB_PORT"),
     }
 }
