db5c03a180eb1c0c44904f12fc0303c0671fbf41
Amulya Jain <jainamulyawin@gmail.com>
2024-09-29 12:58:57 +0530

    Updated pages and components

---
+++ frontend/src/components/Forms.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import api from "../api";
+import { useNavigate } from "react-router-dom";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+
+function Form({route, method}) {
+    
+}
